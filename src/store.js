68cf225e431f7dc406726b92b7b27b6db4e81f02
jacopo-derrico <derricoj.design@gmail.com>
2024-02-19 15:06:31 +0100

    headerNav array list

--- src/store.js
+++ src/store.js
@@ -1,5 +1,52 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    variable: true,
+    headerNav: [
+        {
+            name: 'Home',
+            isDropdown: false,
+            link: '#',
+            dropdownItems: []
+        },
+        {
+            name: 'Shop',
+            isDropdown: false,
+            link: '#',
+            dropdownItems: []
+        },{
+            name: 'Blog',
+            isDropdown: true,
+            link: '#',
+            dropdownItems: [
+                {
+                    name: 'Game World',
+                    link: '#'
+                },
+                {
+                    name: 'Consoles',
+                    link: '#'
+                },
+                {
+                    name: 'New Releases',
+                    link: '#'
+                }
+            ]
+        },{
+            name: 'Media',
+            isDropdown: false,
+            link: '#',
+            dropdownItems: []
+        },{
+            name: 'ShortCode',
+            isDropdown: false,
+            link: '#',
+            dropdownItems: []
+        },
+        {
+            name: 'Features',
+            isDropdown: false,
+            link: '#',
+            dropdownItems: []
+        },
+    ],
 })
