4e3a098076f358b5c1fcbde2b71fcdf6706a6986
jacopo-derrico <derricoj.design@gmail.com>
2024-02-21 13:05:41 +0100

    productsData array information

--- src/store.js
+++ src/store.js
@@ -129,5 +129,79 @@ export const store = reactive({
             gameTitle: 'World Of Tanks New Game',
             img: 'cms-banner-04.jpg'
         }
+    ],
+    productsData: [
+        {
+            img: '01.jpg',
+            imgHover: '02.jpg',
+            vote: 4,
+            name: 'Shopping Mahjong Connect',
+            price: 180.00,
+            discount: 0.44,
+        },
+        {
+            img: '03.jpg',
+            imgHover: '04.jpg',
+            vote: 0,
+            name: 'Buddy and Lucky Solitaire',
+            price: 90.00,
+            discount: null,
+        },
+        {
+            img: '05.jpg',
+            imgHover: '06.jpg',
+            vote: 0,
+            name: 'Taishou x Alice Epilogue',
+            price: 60.00,
+            discount: null,
+        },
+        {
+            img: '07.jpg',
+            imgHover: '08.jpg',
+            vote: 2,
+            name: 'Labyrinths of the World',
+            price: 110.00,
+            discount: null,
+        },
+        {
+            img: '09.jpg',
+            imgHover: '10.jpg',
+            vote: 0,
+            name: 'BangBangShooting',
+            price: 56,
+            discount: null,
+        },
+        {
+            img: '11.jpg',
+            imgHover: '12.jpg',
+            vote: 5,
+            name: 'Connect0r X',
+            price: 200.00,
+            discount: 0.2,
+        },
+        {
+            img: '13.jpg',
+            imgHover: '14.jpg',
+            vote: 1,
+            name: 'The Revolver',
+            price: 30.00,
+            discount: null,
+        },
+        {
+            img: '15.jpg',
+            imgHover: '16.jpg',
+            vote: 3,
+            name: 'Magicka',
+            price: 80.00,
+            discount: 0.1,
+        },
+        {
+            img: '17.jpg',
+            imgHover: '18.jpg',
+            vote: 4,
+            name: 'Oshi No Ko',
+            price: 75.00,
+            discount: 0.37,
+        }
     ]
 })
