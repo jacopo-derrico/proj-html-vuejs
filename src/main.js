dac5e918c4b6144019df2450af31098d5d6c38e7
jacopo-derrico <derricoj.design@gmail.com>
2024-02-21 13:42:05 +0100

    import vuecountdown

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueCountdown from '@chenfengyuan/vue-countdown';
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
